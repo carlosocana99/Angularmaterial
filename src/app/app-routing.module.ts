48f5263327aa3043965c99753952269b1e36ce5a
Carlos Ocaña <c.o.99@hotmail.com>
2021-02-13 11:40:56 +0100

    Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+//importamos los componentes que hemos agregado.
+import { RouterModule, Routes } from '@angular/router';
+import {Pais1Component} from  './pais1/pais1.component';
+import {Pais2Component} from  './pais2/pais2.component';
+import {Pais3Component} from  './pais3/pais3.component';
+
+
+//le añadimos las rutas para poder acceder a cada uno de los componentes,
+const routes: Routes = [
+  {
+    path: 'pais1',
+    component: Pais1Component
+  },
+  {
+    path: 'pais2',
+    component: Pais2Component
+  },
+
+  {
+    path: 'pais3',
+    component: Pais3Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
